b53d80cf0c2e44c487e4ae8ee66a04e5cd895a86
maksym-glushenkov <160600087+maksym-glushenkov@users.noreply.github.com>
2024-04-09 23:10:22 +0200

    added new hw1

---
+++ Lessons 1.1/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello,");
+        System.out.println("my");
+        System.out.println("name");
+        System.out.println("is");
+        System.out.println("Max!");
+
+        System.out.print("Hello, ");
+        System.out.print("my ");
+        System.out.print("name ");
+        System.out.print("is ");
+        System.out.print("Max!");
+        }
+    }
